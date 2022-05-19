970610c5ad66ad6db0a6ba20ca571e25dace7080
Dmitry Tereshchenko <dima123-99@mail.ru>
2022-05-19 16:17:10 +0300

    Fix some mistakes

--- app/views.py
+++ app/views.py
@@ -4,16 +4,16 @@ from app.service import get_nft_img_url, create_tr_mint_nft
 
 @app.route('/')
 def index():
-    # return render_template("home.html", nfts_img={
-    #     'rock_url': get_nft_img_url('Rock'),
-    #     'paper_url': get_nft_img_url('Paper'),
-    #     'scissors_url': get_nft_img_url('Scissors')
-    # })
     return render_template("home.html", nfts_img={
-        'rock_url': "https://ipfs.io/ipfs/bafkreibe6myordolezhyk5m5r76dkitmdq2mopn44thd4m3kxmd3ma4cza",
-        'paper_url': "https://ipfs.io/ipfs/bafkreifkn3whx5ntbxjj3vwrhtxjktzqx35gjsp2c3fceuuanwo57bl72u",
-        'scissors_url': "https://ipfs.io/ipfs/bafkreiax5x6sxnr466akmtvybwcprblcstioji5pe6aw5bfnwsq4wk2o3e"
+        'rock_url': get_nft_img_url('Rock'),
+        'paper_url': get_nft_img_url('Paper'),
+        'scissors_url': get_nft_img_url('Scissors')
     })
+    # return render_template("home.html", nfts_img={
+    #     'rock_url': "https://ipfs.io/ipfs/bafkreibe6myordolezhyk5m5r76dkitmdq2mopn44thd4m3kxmd3ma4cza",
+    #     'paper_url': "https://ipfs.io/ipfs/bafkreifkn3whx5ntbxjj3vwrhtxjktzqx35gjsp2c3fceuuanwo57bl72u",
+    #     'scissors_url': "https://ipfs.io/ipfs/bafkreiax5x6sxnr466akmtvybwcprblcstioji5pe6aw5bfnwsq4wk2o3e"
+    # })
 
 @app.get('/set_wallet/<wallet_address>')
 def set_wallet_session(wallet_address):
